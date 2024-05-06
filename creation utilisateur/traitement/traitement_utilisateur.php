4e2e9e72f6102f3ab693ad6ae5e3fac4c3431a39
CyberPhantomX <151735432+CyberPhantomX@users.noreply.github.com>
2024-05-06 22:29:04 +0200

    Update traitement_utilisateur.php

--- creation utilisateur/traitement/traitement_utilisateur.php
+++ creation utilisateur/traitement/traitement_utilisateur.php
@@ -1,33 +1,4 @@
 <?php
-
-// Télécharge et installe Composer si ce n'est pas déjà fait
-if (!file_exists('composer.phar')) {
-    // Télécharge le script d'installation de Composer
-    copy('https://getcomposer.org/installer', 'composer-setup.php');
-
-    // Vérifie l'intégrité du script téléchargé
-    if (hash_file('sha384', 'composer-setup.php') === 'dac665fdc30fdd8ec78b38b9800061b4150413ff2e3b6f88543c636f7cd84f6db9189d43a81e5503cda447da73c7e5b6') {
-        echo 'Installer verified' . PHP_EOL;
-    } else {
-        echo 'Installer corrupt' . PHP_EOL;
-        unlink('composer-setup.php');
-        exit(1);
-    }
-
-    // Exécute le script d'installation de Composer
-    exec('php composer-setup.php', $output, $returnCode);
-    if ($returnCode !== 0) {
-        echo 'Installation failed' . PHP_EOL;
-        unlink('composer-setup.php');
-        exit(1);
-    }
-
-    // Supprime le script d'installation de Composer
-    unlink('composer-setup.php');
-}
-
-// Maintenant que Composer est installé, vous pouvez utiliser `composer.phar` pour exécuter d'autres commandes Composer si nécessaire
-
 // Inclure l'autoloader de Composer pour charger les classes PhpSpreadsheet
 require 'vendor/autoload.php';
 
